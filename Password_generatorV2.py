7b833f4fff037d0ee491af47d0385bd281c8b048
agipaok <158823680+agipaok@users.noreply.github.com>
2025-10-26 16:47:59 +0200

    Add files via upload
    Added final print + input to exit code

---
+++ Password_generatorV2.py
@@ -0,0 +1,57 @@
+# Password Generator
+
+import random # For random numbers etc.
+
+password = list("abcdefghijklmnopqrstuvwxyz")
+
+upper_letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+nums = list("0123456789")
+
+symbols = list("!@#$%^&*()-_=+[]{}|;:,.<>?/")
+
+
+print("Welcome to Password Generator")
+while True:
+    try:
+        user_pass_length = int(input("How long do you want the password to be?: "))
+        break
+    except ValueError:
+        print("Please give numeric lenght!")
+
+while True:
+    user_pass_caps = input("Do you want to include uppercase letters? (yes/no): ").lower()
+    if user_pass_caps.lower() in ["yes" , "no"]:
+        break
+    else:
+        print("Please enter 'yes' or 'no'.")
+        
+while True:
+    user_pass_nums = input("Do you want to include numbers? (yes/no): ").lower()
+    if user_pass_nums.lower() in ["yes" , "no"]:
+        break
+    else:
+        print("Please enter 'yes' or 'no'.")
+        
+while True:
+    user_pass_symbols = input("Do you want to include symbols? (yes/no): ").lower()
+    if user_pass_symbols.lower() in ["yes" , "no"]:
+        break
+    else:
+        print("Please enter 'yes' or 'no'.")
+
+if user_pass_caps.lower() == "yes":
+    password.extend(upper_letters)
+if user_pass_nums.lower() == "yes":
+    password.extend(nums)
+if user_pass_symbols.lower() == "yes":
+    password.extend(symbols)
+    
+user_password = random.choices(password, k=user_pass_length)
+
+password_string = "".join(user_password)
+
+print(f"Your password is : {password_string}")
+
+input("Πατήστε Enter για έξοδο...")
+
